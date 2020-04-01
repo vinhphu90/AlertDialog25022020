747921ac0d1eb3087721462d754ce8e8d53a60b8
vinhphu90 <61481544+vinhphu90@users.noreply.github.com>
2020-04-01 20:16:59 +0700

    setup Button

--- app/src/main/java/com/examplevinhphutvp/alertdialog25022020/MainActivity.java
+++ app/src/main/java/com/examplevinhphutvp/alertdialog25022020/MainActivity.java
@@ -2,13 +2,30 @@ package com.examplevinhphutvp.alertdialog25022020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button mBtnAlert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtnAlert = findViewById(R.id.buttonAlert);
+
+        mBtnAlert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("thông báo mới");
+                builder.setMessage("bạn vừa nhận tin nhắn mới");
+                builder.setIcon(R.mipmap.ic_launcher);
+                builder.show();
+            }
+        });
     }
 }

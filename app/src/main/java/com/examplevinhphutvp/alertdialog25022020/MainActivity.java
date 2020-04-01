db5237f80496ab301a7470f2afaf388c0cd5dfdd
vinhphu90 <61481544+vinhphu90@users.noreply.github.com>
2020-04-01 20:34:10 +0700

    default alert dialog

--- app/src/main/java/com/examplevinhphutvp/alertdialog25022020/MainActivity.java
+++ app/src/main/java/com/examplevinhphutvp/alertdialog25022020/MainActivity.java
@@ -3,9 +3,11 @@ package com.examplevinhphutvp.alertdialog25022020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button mBtnAlert;
@@ -25,6 +27,34 @@ public class MainActivity extends AppCompatActivity {
                 builder.setMessage("bạn vừa nhận tin nhắn mới");
                 builder.setIcon(R.mipmap.ic_launcher);
                 builder.show();
+                builder.setCancelable(false);
+
+                //Button xác nhận
+                builder.setPositiveButton("có", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this,"có", Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                    }
+                });
+
+                //Button từ chối
+                builder.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this,"Không", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                //Button hủy
+                builder.setNeutralButton("Hủy", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this,"Hủy", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                builder.show();
+
             }
         });
     }
